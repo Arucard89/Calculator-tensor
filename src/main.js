169fe03c60cfb7a95be38f65e65c9af94c3a7b42
Kirill Revenkov <revenkov-k@mail.ru>
2018-11-27 22:13:28 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 
 class BigNumberOperations {
 
-    constructor(a = 0, b = 0){
-        this.a = a;
-        this.b = b;
+    constructor(a = '0', b = '0'){
+        this.a = a.trim();
+        this.b = b.trim();
     }
 
     checkNumber(s){
-        let regExp = /^(\+|-)?[0-9]+/g;
-        return (s.length > 64) && (regExp.test(s));
+        let regExp = /^((\+|-)?)(\d+)$/g;
+        return (s.length <= 64) && (regExp.test(s));
     }
 
 }
+
+let bn = new BigNumberOperations();
+console.log(bn.checkNumber(''));
